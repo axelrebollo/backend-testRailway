080688388cf7e54f431df1a573033f62f92f6784
Axel <axel.rebollo.cepero@gmail.com>
2025-11-07 18:43:28 +0100

    proyecto backend inicial con spring boot

---
+++ src/main/java/com/testrailway/backendtestrailway/BackendTestRailwayApplication.java
@@ -0,0 +1,13 @@
+package com.testrailway.backendtestrailway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTestRailwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTestRailwayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testrailway/backendtestrailway/BackendTestRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testrailway.backendtestrailway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTestRailwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
